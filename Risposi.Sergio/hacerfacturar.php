29f832a38f025c023220cf5deed255139a91db92
srisposi <54085792+srisposi@users.noreply.github.com>
2019-10-08 19:48:54 -0300

    Funcionando Facturados.txt
    Falta eliminar de la lista los ya facturados

--- Risposi.Sergio/hacerfacturar.php
+++ Risposi.Sergio/hacerfacturar.php
@@ -25,17 +25,42 @@ var_dump($checkPatente);
 							 	
 			{
 				$Bandera=0;
-				//echo "<br>es:";
+				
+
+
+			    //echo "<br>es:";
 				//var_dump($objeto);
 				$diff=($horaSalida-$objeto->fechaIngreso);
 				$pago=$precio*$diff;
+
+
+
+				date_default_timezone_set('America/Argentina/Buenos_Aires');
+
+				$objetoFacturado=new stdClass();
+
+				$objetoFacturado->patenteFacturada = $checkPatente;
+			    $objetoFacturado->fechaEntrada = date("d-m-y H:i",$objeto->fechaIngreso);
+			    $objetoFacturado->fechaSalida = date("d-m-y H:i",$horaSalida);
+			    $objetoFacturado->importe = $pago;
+
+
+
+
+				$archivo1 = fopen('facturados.txt', 'a');
+			       fwrite($archivo1, json_encode($objetoFacturado)."\n");
+			       fclose($archivo1);
+
+
+
 				//var_dump($horaSalida);
 				//var_dump($objeto->fechaIngreso);				
 				//die();
 				//header("Location: Exito.php?fechaIngreso=".$objeto->fechaIngreso);
 				header("Location: Exito.php?pago=".$pago);
 				exit();
-			
+
+						
 			}
 
 		}
@@ -46,22 +71,7 @@ var_dump($checkPatente);
 if($Bandera==1)
 {
 
-
-	date_default_timezone_set('America/Argentina/Buenos_Aires');
-
-	$objetoFacturado=new stdClass();
-
-	$objetoFacturado->patenteFacturada = $checkPatente;
-       $objetoFacturado->fechaEntrada = date("d-m-y H:i",$objeto->fechaIngreso);
-       $objetoFacturado->fechaSalida = date("d-m-y H:i",$horaSalida);
-       $objetoFacturado->importe = $pago;
-
-
-
-
-	$archivo1 = fopen('facturados.txt', 'a');
-       fwrite($archivo1, json_encode($objetoFacturado)."\n");
-       fclose($archivo1);		
+		
 
 	//die();
 	header("Location: Error.php");
