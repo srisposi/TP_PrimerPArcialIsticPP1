4eac5dc12b21e275edd73e3346aa7e76dcd198bf
srisposi <54085792+srisposi@users.noreply.github.com>
2019-11-05 21:51:53 -0300

    Base de Datos Funcionando
    Base de Datos Funcionando hasta Registro y Login.

--- Risposi.Sergio/funciones/hacerRegistro.php
+++ Risposi.Sergio/funciones/hacerRegistro.php
@@ -1,6 +1,6 @@
 <?php
 
-$miObjeto = new stdClass();
+/*$miObjeto = new stdClass();
 $miObjeto->nombre = $_GET['inputUsuario'];
 $miObjeto->contraseña = $_GET['inputPassword'];
 
@@ -9,7 +9,26 @@ fwrite($archivo, json_encode($miObjeto)."\n");
 
 fclose($archivo);
 header("Location: ../paginas/registro.php?exito=exito");
+*/
 
 
+
+include 'AccesoDatos.php';
+$miObjeto = new stdClass();
+$miObjeto->nombre = $_GET['inputUsuario'];
+$miObjeto->apellido = $_GET['inputPassword'];
+$objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+$select="INSERT INTO usuario( nombre, clave) VALUES ('$miObjeto->nombre','$miObjeto->apellido')";
+$consulta =$objetoAccesoDato->RetornarConsulta($select);
+$consulta->execute();
+
+header("Location: ../paginas/registro.php?exito=exito");
+
+/*INSERT INTO usuario( nombre, clave) VALUES ("natalia","natalia")
+
+$archivo = fopen('usuarios.txt', 'a');
+fwrite($archivo, json_encode($miObjeto)."\n");
+fclose($archivo);*
+*/
 
 ?>
